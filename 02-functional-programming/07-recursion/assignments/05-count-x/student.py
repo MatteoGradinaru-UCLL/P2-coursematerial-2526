02d217f743af8b1ccf8c3d9423b9168fade7b1c6
Matteo Gradinaru <matteo.gradinaru@student.ucll.be>
2026-06-08 10:08:24 +0200

    more for recursion

--- 02-functional-programming/07-recursion/assignments/05-count-x/student.py
+++ 02-functional-programming/07-recursion/assignments/05-count-x/student.py
@@ -1,22 +1,12 @@
 # def countX(text):
-#     if len(text) == 0:
+#     if len(text) < 1:
 #         return 0
-
+    
 #     if text[0] == "x":
 #         return 1 + countX(text[1:])
 #     else:
 #         return 0 + countX(text[1:])
-
-
-# print(countX("axa"))
-
-
-
-
-
-
-
-
+    
 
 
 def countX(text):
